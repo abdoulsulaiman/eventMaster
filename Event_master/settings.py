39e8d57b884fc8eb0a28ed0a1584e5cb4f52b3e1
sdushimimana <sdushimimana@bk.rw>
2024-04-08 12:50:14 +0300

    added changes in settings

--- Event_master/settings.py
+++ Event_master/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import environ
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,12 +27,15 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)h9m@m03lcb@5b-0)fu1cz0y!$o#clkboyzf+fj5qef%kn3*n#'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+ 
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG","False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ 
 
 
 # Application definition
@@ -85,17 +89,25 @@ WSGI_APPLICATION = 'Event_master.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'event_master_db', 
+#         'USER': 'postgres',
+#         'PASSWORD': 'root',
+#         'HOST': '127.0.0.1', 
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'event_master_db', 
-        'USER': 'postgres',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR /'db.sqlite3', 
     }
 }
-
+ 
+ 
+DATABASES['default'] = dj_database_url.parse("postgres://event_master_db_user:Dui8FzALKUfD3ujRO7CkPZYVskW0PB9D@dpg-co9r63q0si5c739hj300-a.oregon-postgres.render.com/event_master_db")
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
